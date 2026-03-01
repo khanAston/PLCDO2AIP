d88d9bc3434b6ba5474429d21903ae829bdb836c
Samdoses <106073273+Samdoses@users.noreply.github.com>
2026-03-01 19:12:16 +0000

    completed task 2c in python

--- lab6-collections/python/playlistColls.py
+++ lab6-collections/python/playlistColls.py
@@ -58,7 +58,7 @@ things = [111, piece1, "hello", advert1, [piece2]]
 
 print("things = %s" % things)
 
-piecesFromThings = "todo" #TASK 6.2-c -- replace "todo" by a list comprehension
+piecesFromThings = [piece for piece in things if piece.__class__ == Piece]
 
 print("piecesFromThings = %s" % piecesFromThings)
 
